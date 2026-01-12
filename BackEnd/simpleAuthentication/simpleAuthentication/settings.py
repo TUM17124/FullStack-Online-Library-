73f8138b9b95f053fac4cdd2ab779d5d59259660
TUM17124 <tumgodwinkiprotich@gmail>
2026-01-12 23:33:59 +0300

    commit final changes

--- BackEnd/simpleAuthentication/simpleAuthentication/settings.py
+++ BackEnd/simpleAuthentication/simpleAuthentication/settings.py
@@ -23,11 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i*)-xk(c5r9%p7&&(mfz+k!qqnf9!wa%yp*l76gi$a!rma&s1j'
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "mindandmoney.onrender.com",
+    "online-library-frontend.onrender.com",
+]
 
 
 
@@ -148,6 +151,11 @@ CORS_ALLOWED_ORIGINS = [
     "https://online-library-frontend.onrender.com",
     "http://localhost:3000",
 ]
+CSRF_TRUSTED_ORIGINS = [
+    "https://mindandmoney.onrender.com",
+    "https://online-library-frontend.onrender.com",
+]
+
 
 
 REST_FRAMEWORK = {
