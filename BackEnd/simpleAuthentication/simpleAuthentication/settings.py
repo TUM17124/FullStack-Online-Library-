475f3fe7b993a3cefdf460f446418147c7e31a44
TUM17124 <tumgodwinkiprotich@gmail>
2026-02-01 01:06:01 +0300

    l

--- BackEnd/simpleAuthentication/simpleAuthentication/settings.py
+++ BackEnd/simpleAuthentication/simpleAuthentication/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "my-local-dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
