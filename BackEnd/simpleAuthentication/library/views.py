513312f0fb81e75ab1330c7be558750dd8d13551
TUM17124 <tumgodwinkiprotich@gmail>
2026-05-09 15:13:45 +0300

    okey

--- BackEnd/simpleAuthentication/library/views.py
+++ BackEnd/simpleAuthentication/library/views.py
@@ -13,7 +13,6 @@ from django.utils.encoding import smart_str
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
-import requests
 from rest_framework import status, serializers
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -129,39 +128,20 @@ class ReadBookView(APIView):
     def get(self, request, book_id):
         book = get_object_or_404(Book, id=book_id)
 
-        if not Borrow.objects.filter(
-            user=request.user,
-            book=book,
-            returned=False
-        ).exists():
+        if not Borrow.objects.filter(user=request.user, book=book, returned=False).exists():
             return HttpResponseForbidden("You must borrow this book to read it.")
 
         if not book.file:
             return Response({"error": "Book file not available"}, status=404)
 
-        file_path = str(book.file).lstrip("/")
+        file_path = str(book.file)
 
-        # if already full URL
         if file_path.startswith("http"):
             file_url = file_path
         else:
-            file_url = (
-                f"{settings.SUPABASE_URL}"
-                f"/storage/v1/object/public/media/{file_path}"
-            )
-            print("FILE PATH:", file_path)
-            print("SUPABASE FINAL URL:", file_url)
+            file_url = f"{settings.SUPABASE_URL}/storage/v1/object/public/{file_path}"
 
-        response = requests.get(file_url, stream=True)
-        if response.status_code != 200:
-            return Response({"error": "Failed to retrieve book file"}, status=404)
-        pdf_response = HttpResponseForbidden(
-            response.content,
-            content_type='application/pdf'
-        )
-        pdf_response['Content-Disposition'] = f'inline; filename="{smart_str(book.title)}.pdf"'
-        return pdf_response
-        
+        return Response({"url": file_url})
 
 class OverdueBooksView(APIView):
     permission_classes = [IsAuthenticated]
